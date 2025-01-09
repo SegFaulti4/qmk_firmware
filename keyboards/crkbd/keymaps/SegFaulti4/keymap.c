cae1c42042b81e656d8601d52b72601284861d4c
Makar Popov <makar.popov01@mail.ru>
2025-01-09 20:51:26 +0300

    swapped layers, removed symmetry

--- keyboards/crkbd/keymaps/SegFaulti4/keymap.c
+++ keyboards/crkbd/keymaps/SegFaulti4/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM nm_combo[] = {KC_N, KC_M, COMBO_END};
 const uint16_t PROGMEM asdf_combo[] = {KC_A, KC_S, KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM jklS_combo[] = {KC_J, KC_K, KC_L, KC_SCLN, COMBO_END};
 
-const uint16_t PROGMEM dsft_combo[] = {KC_LSFT, KC_RSFT, COMBO_END};
+const uint16_t PROGMEM edik_combo[] = {KC_E, KC_D, KC_I, KC_K, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(qa_combo, KC_LALT),
@@ -63,33 +63,33 @@ combo_t key_combos[] = {
     COMBO(asdf_combo, KC_NO),
     COMBO(jklS_combo, KC_NO),
 
-    COMBO(dsft_combo, KC_CAPS),
+    COMBO(edik_combo, KC_CAPS),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(
+   [0] = LAYOUT_split_3x6_3(
       KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,        KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_NO,
       KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_NO,
       KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
             KC_ESC, KC_TAB, LT(1, KC_SPC),        LT(2, KC_BSPC), KC_ENT, KC_ESC
 
-  ),
-    [1] = LAYOUT_split_3x6_3(
-      KC_NO, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,         LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), KC_NO,
-      KC_NO, KC_6,  KC_7,  KC_8,  KC_9,  KC_0,         LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-                         KC_NO, KC_NO, KC_TRNS,        MO(3), KC_NO, KC_NO
-  ),
-    [2] = LAYOUT_split_3x6_3(
-      KC_NO, KC_LEFT,       KC_DOWN,       KC_UP,         KC_RGHT,       KC_NO,        KC_NO, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
-      KC_NO, LSFT(KC_LBRC), LSFT(KC_RBRC), LSFT(KC_RBRC), LSFT(KC_RBRC), KC_NO,        KC_NO, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_NO,
-      KC_NO, KC_NO,         LSFT(KC_GRV),  LSFT(KC_QUOT), LSFT(KC_BSLS), KC_NO,        KC_NO, KC_NO,   KC_GRV,  KC_QUOT, KC_BSLS, KC_NO,
-                                                           KC_NO, KC_NO, MO(3),        KC_TRNS, KC_NO, KC_NO
-  ),
-    [3] = LAYOUT_split_3x6_3(
+   ),
+   [1] = LAYOUT_split_3x6_3(
+      KC_NO, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5),        KC_NO, LSFT(KC_MINS), LSFT(KC_EQL),  LST(KC_LBRC),  LSFT(KC_RBRC), KC_NO,
+      KC_NO, LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0),        KC_NO, KC_LEFT,       KC_DOWN,       KC_UP,         KC_RIGHT,      KC_NO,
+      KC_NO, KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,             KC_NO, LSFT(KC_GRV),  LSFT(KC_QUOT), LSFT(KC_BSLS), KC_NO,         KC_NO,
+                                                  KC_NO, KC_NO, KC_TRNS,        MO(3), KC_NO, KC_NO
+   ),
+   [2] = LAYOUT_split_3x6_3(
+      KC_NO, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,         KC_NO, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_NO,
+      KC_NO, KC_6,  KC_7,  KC_8,  KC_9,  KC_0,         KC_NO, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_GRV,  KC_QUOT, KC_BSLS, KC_NO,    KC_NO,
+                           KC_NO, KC_NO, MO(3),        KC_TRNS, KC_NO, KC_NO
+   ),
+   [3] = LAYOUT_split_3x6_3(
       KC_NO, KC_F1,  KC_F2,  KC_F3, KC_F4, KC_F5,         KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
       KC_NO, KC_F6,  KC_F7,  KC_F8, KC_F9, KC_F10,        KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_PSCR, KC_NO,
       KC_NO, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                            KC_NO, KC_NO, KC_TRNS,         KC_TRNS, KC_NO, KC_NO
-  )
+   )
 };
