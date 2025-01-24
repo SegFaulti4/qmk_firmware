b6a704c6c710e4d1dbc9d267fc70bf6b219516e4
Makar Popov <makar.popov01@mail.ru>
2025-01-24 21:09:02 +0300

    add gaming layer

--- keyboards/crkbd/keymaps/3x6/keymap.c
+++ keyboards/crkbd/keymaps/3x6/keymap.c
@@ -79,9 +79,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                          KC_NO,    KC_NO,    MO(3), /*****************/ KC_TRNS,    KC_NO,    KC_NO
    ),
    [3] = LAYOUT_split_3x6_3(
-      KC_NO, KC_NO, KC_F1, KC_F2,  KC_F3,  KC_F4, /******************/ KC_NO, KC_PSCR, KC_INS,  KC_DEL,  KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_F5, KC_F6,  KC_F7,  KC_F8, /******************/ KC_NO, KC_NO,   KC_CAPS, KC_NO,   KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, /*****************/ KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_F1, KC_F2,  KC_F3,  KC_F4, /******************/ KC_NO, KC_PSCR, KC_INS,  KC_DEL,  KC_NO, DF(4),
+      KC_NO, KC_NO, KC_F5, KC_F6,  KC_F7,  KC_F8, /******************/ KC_NO, KC_NO,   KC_CAPS, KC_NO,   KC_NO, DF(4),
+      KC_NO, KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, /*****************/ KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, DF(4),
                       KC_NO,    KC_NO,    KC_TRNS, /*****************/ KC_TRNS,    KC_NO,    KC_NO
+   ),
+   [4] = LAYOUT_split_3x6_3(
+      KC_T, KC_TAB,  KC_Q, KC_W, KC_E, KC_R, /*****************/ KC_Y, KC_U, KC_I,    KC_O,   KC_P,    DF(0),
+      KC_G, KC_LSFT, KC_A, KC_S, KC_D, KC_F, /*****************/ KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, DF(0),
+      KC_B, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, /*****************/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, DF(0),
+              KC_ESC,    KC_LALT,    KC_SPC, /*****************/ KC_BSPC,    KC_ENT,    KC_LGUI
    )
 };
