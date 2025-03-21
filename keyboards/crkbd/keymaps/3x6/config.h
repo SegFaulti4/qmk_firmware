c6e0944d1bfe585f2fde60438b1c7a600558d712
Makar Popov <makar.popov01@mail.ru>
2025-03-21 14:14:18 +0300

    increase combo term

--- keyboards/crkbd/keymaps/3x6/config.h
+++ keyboards/crkbd/keymaps/3x6/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define ONESHOT_TAP_TOGGLE 2
 #define ONESHOT_TIMEOUT 2000
-#define COMBO_TERM 200
+#define COMBO_TERM 300
 #define TAPPING_TERM 150
 #define PERMISSIVE_HOLD
 #define COMBO_ONLY_FROM_LAYER 0
