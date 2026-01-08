afb1056df0cbb9b275236d77478900cddf8a237e
Makar Popov <makar.popov01@mail.ru>
2026-01-08 21:46:41 +0300

    add gaming layer for sweep

--- keyboards/ferris/keymaps/3x5/keymap.c
+++ keyboards/ferris/keymaps/3x5/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q, KC_W,         KC_E,         LALT_T(KC_R), KC_T, /*****************/ KC_Y, RALT_T(KC_U), KC_I,         KC_O,         KC_P,
       KC_A, LGUI_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, /*****************/ KC_H, RCTL_T(KC_J), RSFT_T(KC_K), RGUI_T(KC_L), KC_SCLN,
       KC_Z, KC_X,         KC_C,         LT(4, KC_V),  KC_B, /*****************/ KC_N, LT(5, KC_M),  KC_COMM,      KC_DOT,       KC_SLSH,
-                                           KC_ESC,    TT(1), /*****************/ TT(2),    KC_CAPS
+                                          KC_ESC,    TT(1), /*****************/ TT(2),    KC_CAPS
    ),
    [1] = LAYOUT_split_3x5_2(
       KC_NO,      MS_WHLL,    MS_WHLR,    KC_LALT,    KC_NO,      /*****************/ KC_NO, KC_1, KC_2, KC_3, KC_NO,
@@ -31,12 +31,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,  MS_WHLD, KC_UP,   MS_WHLU,  KC_NO, /*****************/ KC_NO, KC_RALT, MS_WHLL, MS_WHLR, KC_NO,
       KC_NO,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, /*****************/ KC_NO, KC_RCTL, KC_RSFT, KC_RGUI, KC_NO,
       KC_END, KC_PGDN, KC_PGUP, KC_HOME,  KC_NO, /*****************/ KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                       KC_NO,    MO(3), /*****************/ TG(2),    KC_NO
+                                KC_NO,    MO(3), /*****************/ TG(2),    KC_NO
    ),
    [3] = LAYOUT_split_3x5_2(
-      KC_NO, KC_F1, KC_F2, KC_F3, KC_NO, /*****************/ KC_NO, KC_F10, KC_F11,  KC_F12,  KC_PSCR,
-      KC_NO, KC_F4, KC_F5, KC_F6, KC_NO, /*****************/ KC_NO, KC_INS, KC_HOME, KC_PGUP, KC_VOLU,
-      KC_NO, KC_F7, KC_F8, KC_F9, KC_NO, /*****************/ KC_NO, KC_DEL, KC_END,  KC_PGDN, KC_VOLD,
+      DF(6), KC_F1, KC_F2, KC_F3, KC_NO, /*****************/ KC_NO, KC_F10, KC_F11,  KC_F12,  KC_PSCR,
+      DF(6), KC_F4, KC_F5, KC_F6, KC_NO, /*****************/ KC_NO, KC_INS, KC_HOME, KC_PGUP, KC_VOLU,
+      DF(6), KC_F7, KC_F8, KC_F9, KC_NO, /*****************/ KC_NO, KC_DEL, KC_END,  KC_PGDN, KC_VOLD,
                       KC_NO,    KC_TRNS, /*****************/ KC_TRNS,    KC_NO
    ),
    [4] = LAYOUT_split_3x5_2(
@@ -50,6 +50,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,       LSFT(KC_4), LSFT(KC_5), LSFT(KC_6), KC_NO, /*****************/ KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
       LSFT(KC_0),   LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), KC_NO, /*****************/ KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO,
                                               KC_NO,    KC_NO, /*****************/ KC_NO,    KC_NO
+   ),
+   [6] = LAYOUT_split_3x5_2(
+      KC_TAB,  KC_Q, KC_W, KC_E, KC_R, /*****************/ KC_T, KC_Y, KC_U, KC_I,    DF(0),
+      KC_CAPS, KC_A, KC_S, KC_D, KC_F, /*****************/ KC_G, KC_H, KC_J, KC_K,    DF(0),
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, /*****************/ KC_B, KC_N, KC_M, KC_COMM, DF(0),
+                   KC_LALT,    KC_SPC, /*****************/ KC_ESC,    KC_ENT
    )
    //[X] = LAYOUT_split_3x5_2(
    //   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /*****************/ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
